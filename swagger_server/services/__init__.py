53b6ad0d17bc468afd8f4dd35d4a43b6ada0bd15
Eric <ericvdwoude@gmail.com>
2021-02-11 10:26:42 +0100

    Renamed folder

--- swagger_server/services/__init__.py
+++
@@ -1,6 +0,0 @@
-# coding: utf-8
-
-# flake8: noqa
-from __future__ import absolute_import
-# import services into service package
-from swagger_server.services.student_service import add_student, get_student_by_id, delete_student
