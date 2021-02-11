c11d3780260c5a25083db9cd9dcef940592aeee7
Eric <ericvdwoude@gmail.com>
2021-02-11 15:41:31 +0100

    Fix util.py

--- swagger_server/util.py
+++ swagger_server/util.py
@@ -6,10 +6,9 @@ import typing
 
 def _deserialize(data, klass):
     """Deserializes dict, list, str into an object.
-
+    
     :param data: dict, list or str.
     :param klass: class literal, or string of class name.
-
     :return: object.
     """
     if data is None:
@@ -23,10 +22,10 @@ def _deserialize(data, klass):
         return deserialize_date(data)
     elif klass == datetime.datetime:
         return deserialize_datetime(data)
-    elif type(klass) == typing.GenericMeta:
-        if klass.__extra__ == list:
+    elif hasattr(klass, '__origin__'):
+        if klass.__origin__ == list:
             return _deserialize_list(data, klass.__args__[0])
-        if klass.__extra__ == dict:
+        if klass.__origin__ == dict:
             return _deserialize_dict(data, klass.__args__[1])
     else:
         return deserialize_model(data, klass)
