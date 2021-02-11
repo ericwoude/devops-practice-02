ff15453012493bed6baeb1e52520d9fdafb21552
Eric <ericvdwoude@gmail.com>
2021-02-11 11:06:45 +0100

    Fix test #3

--- swagger_server/services/student_service.py
+++ swagger_server/services/student_service.py
@@ -34,9 +34,16 @@ def get_student_by_id(student_id, subject):
     student = student_db.get(doc_id=int(student_id))
     if not student:
         return student
+
     student = Student.from_dict(student)
+
     if not subject:
         return student
+
+    if subject in student.grades:
+        return student
+
+    return None
 
 
 def delete_student(student_id):
