c4e4f590015b521e1e6b3199bc4880e52f28d778
Eric <ericvdwoude@gmail.com>
2021-02-11 11:06:45 +0100

    Fix test #3

--- swagger_server/services/student_service.py
+++ swagger_server/services/student_service.py
@@ -16,6 +16,9 @@ student_db = TinyDB(db_file_path)
 
 
 def add_student(student):
+    if not student.name or not student.last_name:
+        return 'method not allowed', 405
+
     queries = []
     query = Query()
     queries.append(query.first_name == student.first_name)
@@ -34,9 +37,16 @@ def get_student_by_id(student_id, subject):
     student = student_db.get(doc_id=int(student_id))
     if not student:
         return student
+
     student = Student.from_dict(student)
+
     if not subject:
         return student
+
+    if subject in student.grades:
+        return student
+
+    return None
 
 
 def delete_student(student_id):
